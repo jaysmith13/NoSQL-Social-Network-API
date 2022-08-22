bbcd59207103d2cf66f859a35c9a8971f37b171b
jaysmith13 <janeasmith13@gmail.com>
2022-08-22 01:06:37 -0600

    Update thought-controller.js
    added get all thoughts function

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,1 +1,17 @@
 const {thought, user} = require("../models");
+const thoughtController = {
+        getAllThought(req, res) {
+            Thought.find({})
+              .populate({
+                path: "reactions",
+                select: "-__v",
+              })
+              .select("-__v")
+              .sort({ _id: -1 })
+              .then((dbThoughtData) => res.json(dbThoughtData))
+              .catch((err) => {
+                res.status(400).json(err);
+              });
+            }}
+
+module.exports = thoughtController;
