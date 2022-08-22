ddb37bf1c308569abbb7ed01b07d89d621c7047b
jaysmith13 <janeasmith13@gmail.com>
2022-08-22 00:58:31 -0600

    Update index.js
    add mongoose functions

--- models/index.js
+++ models/index.js
@@ -10,3 +10,14 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/NoSQL-Social-Network-API', {
+  useUnifiedTopology: true,  
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  
+});
+
+mongoose.set('debug', true);
+
+
+app.listen(PORT, () => console.log(`You're now Connected on localhost:${PORT}`));
