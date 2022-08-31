5491e26dd718b4b48f86af72e04f0453cbc086cf
jaysmith13 <janeasmith13@gmail.com>
2022-08-30 23:32:10 -0600

    Update thought.js
    add and fix functions

--- models/thought.js
+++ models/thought.js
@@ -1,1 +1,72 @@
 const { Schema, model, Types } = require("mongoose");
+const { format } = require("date-fns");
+
+//creating schema for reactions
+const ReactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: "true",
+        trim: true,
+        maxLength: 280,
+      },
+      username: {
+        type: String,
+        required: "true",
+        trim: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => format(createdAtVal, "PPpp"),//Using date-fns to format
+      },
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+      id: false,
+    }
+  );
+  
+  const ThoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => format(createdAtVal, "PPpp"),
+      },
+      username: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      reactions: [ReactionSchema],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+    }
+  );
+  
+  ThoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
+  
+
+  const Thought = model("Thought", ThoughtSchema);
+  
+  module.exports = Thought;
